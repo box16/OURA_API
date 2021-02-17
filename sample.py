a5e6fd3535657d1a6e9724ffb63b14dea18a4f67
box16 <inoinobox16@gmail.com>
2021-02-17 18:33:01 +0900

    mets-頻度の作成

--- sample.py
+++ sample.py
@@ -2,9 +2,10 @@ import requests
 import json
 import os
 import datetime
+import collections
+import pprint
 
 BASE_URL = "https://api.ouraring.com/v1/"
-API_LIST = ["userinfo","sleep","activity","readiness"]
 TOKEN    = "access_token=" + str(os.environ.get("OURA_TOKEN"))
 
 def create_date_str(weeks_offset=0):
@@ -16,11 +17,23 @@ def create_date_str(weeks_offset=0):
 def create_url(weeks_offset=0):
     end_date = create_date_str(weeks_offset)
     start_date = create_date_str(weeks_offset+1)
-    url = f"{BASE_URL}{API_LIST[2]}?start={start_date}&end={end_date}&{TOKEN}"
+    url = f"{BASE_URL}activity?start={start_date}&end={end_date}&{TOKEN}"
     return url
 
-headers = {"content-type": "application/json"}
-responce = requests.get(create_url(0), headers=headers)
-data = responce.json()
-for activity in data["activity"]:
-    print(activity["score"])
+if __name__ == "__main__":
+    headers = {"content-type": "application/json"}
+    mets_counter = collections.Counter()
+    
+    for week_offset in range(5):
+        responce = requests.get(create_url(week_offset), headers=headers)
+        data = responce.json()
+        for activity in data["activity"]:
+            mets_counter.update(activity["met_1min"])
+
+    total_num = sum(mets_counter.values())
+    for mets,count in mets_counter.items():
+        mets_counter[mets] = count/total_num
+
+    with open("mets.txt","w") as f:
+        for mets,count in mets_counter.items():
+            f.write(f"{mets}\t{count}\n")
