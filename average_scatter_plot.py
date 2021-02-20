89df21e74d6c6bbdd1df8ed821339ab58c7b5c50
box16 <inoinobox16@gmail.com>
2021-02-20 21:51:35 +0900

    metsとかのグラフ作成

--- average_scatter_plot.py
+++ average_scatter_plot.py
@@ -23,13 +23,12 @@ os.mkdir("figure")
 
 def convert_column_list(column):
     if "," in column:
-        return column.split(",")
+        return [float(word) for word in column.split(",")]
     else:
-        return [word for word in column]
+        return [float(word) for word in column]
 
 
 def create_graph(_list, column):
-    _list = [float(i) for i in _list]
     fig = plt.figure()
     plt.scatter(range(1, len(_list) + 1), _list)
     fig.savefig(
@@ -37,9 +36,22 @@ def create_graph(_list, column):
     print(f"{column} create!")
 
 
+def average_list(picks):
+    result = convert_column_list(picks[0])
+    picks_num = 0
+    for pick in picks[1::]:
+        picks_num += 1
+        for index, value in enumerate(convert_column_list(pick)):
+            try:
+                result[index] += value
+            except IndexError:
+                result.append(value)
+    return [value / picks_num for value in result]
+
+
 if __name__ == "__main__":
     for table, columns in target_list.items():
         for column in columns:
-            pick = db_api.pick_column(table, column)
-            _list = convert_column_list(pick[0])
+            picks = db_api.pick_column(table, column)
+            _list = average_list(picks)
             create_graph(_list, column)
