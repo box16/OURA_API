1c12d3aae9552669e8518816984fe7deb47651db
box16 <inoinobox16@gmail.com>
2021-02-20 14:19:41 +0900

    収集

---
+++ data_collecter.py
@@ -0,0 +1,38 @@
+from oura_access import OuraAccess
+from db_api import DBapi
+import datetime
+import re
+
+
+def format_activity(activity):
+    activity["met_1min"] = "'" + \
+        ",".join([str(met) for met in activity["met_1min"]]) + "'"
+    return activity
+
+
+def format_sleep(sleep):
+    sleep["hr_5min"] = "'" + ",".join([str(hr)
+                                       for hr in sleep["hr_5min"]]) + "'"
+    sleep["rmssd_5min"] = "'" + \
+        ",".join([str(rm) for rm in sleep["rmssd_5min"]]) + "'"
+    sleep["bedtime_start"] = f"'{sleep['bedtime_start'][11:19]}'"
+    sleep["bedtime_end"] = f"'{sleep['bedtime_end'][11:19]}'"
+    return sleep
+
+
+def format_data(target, data):
+    if target == "activity":
+        return format_activity(data)
+    elif target == "sleep":
+        return format_sleep(data)
+    return data
+
+
+oura = OuraAccess()
+db_api = DBapi()
+
+for day_offset in range(1, 30):
+    for target in ["activity", "sleep", "readiness"]:
+        data = oura.collect_daily_summaries(target, day_offset)
+        db_api.regist_data(target, data[0], format_data(target, data[1]))
+    print(day_offset)
